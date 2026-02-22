35b0421efb5d2149e0de0ce4beb4d459f7ed149e
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-02-21 20:48:07 -0500

    Implementación inicial de la clase Virus

---
+++ tcp-server/src/main/java/org/JuanDiego/models/Virus.java
@@ -0,0 +1,53 @@
+package org.JuanDiego.models;
+
+import enums.InfectionLevel;
+
+/**
+ * Clase que representa un virus
+ * @author Juan Diego
+ * @since 20260221
+ * @version 1.0
+ */
+public class Virus {
+
+    ////// Atributos
+
+    /**
+     * Representa el nombre del virus
+     */
+    private String name;
+
+    /**
+     * Representa el nivel de infeccion del virus con el Enum InfectionLevel
+     */
+    private InfectionLevel infectionLevel;
+
+    /**
+     * Representa la secuencia genetica del virus
+     */
+    private String geneticSequence;
+
+
+    ////// Metodo constructor
+    public Virus(String name, InfectionLevel infectionLevel, String geneticSequence) {
+        this.name = name;
+        this.infectionLevel = infectionLevel;
+        this.geneticSequence = geneticSequence;
+    }
+
+
+
+    ////// Metodos de acceso
+
+    public String getName() {
+        return name;
+    }
+
+    public InfectionLevel getInfectionLevel() {
+        return infectionLevel;
+    }
+
+    public String getGeneticSequence() {
+        return geneticSequence;
+    }
+}
