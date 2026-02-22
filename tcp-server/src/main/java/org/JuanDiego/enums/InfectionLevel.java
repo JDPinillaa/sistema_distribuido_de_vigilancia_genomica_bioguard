d944c75d06093dba6b135ec3770591809238676c
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-02-22 13:22:25 -0500

    Implementacion de metodos para la creacion de instancias de Virus desde archivos en formato fasta

--- tcp-server/src/main/java/org/JuanDiego/enums/InfectionLevel.java
+++ tcp-server/src/main/java/org/JuanDiego/enums/InfectionLevel.java
@@ -4,15 +4,15 @@ package org.JuanDiego.enums;
  * Enum que representa el nivel de infeccion de un virus
  * @author Juan Diego
  * @since 20260221
- * @version 1.0
+ * @version 1.1
  */
 public enum InfectionLevel {
 
     ////// Niveles de infeccion
 
-    Nivel_bajo("Nivel de infeccion poco infeccioso"),
-    Nivel_medio("Nivel de infeccion medianamente infeccioso"),
-    Nivel_Alto("Nivel de infeccion altamente infeccioso");
+    Nivel_bajo("Poco infeccioso"),
+    Nivel_medio("Medianamente infeccioso"),
+    Nivel_Alto("Altamente infeccioso");
 
 
     InfectionLevel(String description) {
@@ -30,5 +30,18 @@ public enum InfectionLevel {
      */
     public String getDescription() {
         return description;
+    }
+
+
+    /**
+     * Metodo que crea una instancia de este Enum obteniendo su descripción desde una cadena
+     */
+    public static InfectionLevel fromString(String text){
+        for(InfectionLevel il : InfectionLevel.values()){
+            if(il.description.equalsIgnoreCase(text.trim())){
+                return il;
+            }
+        }
+        throw new IllegalArgumentException("Nivel de infeccion del virus no valido");
     }
 }
